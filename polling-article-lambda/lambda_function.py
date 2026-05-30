9d993f923d1cb529a9750f13683c48afddbada7b
rlhill888 <rlhill888@gmail.com>
2026-05-29 21:26:48 -0400

    setting up lambda

--- polling-article-lambda/lambda_function.py
+++ polling-article-lambda/lambda_function.py
@@ -1,8 +1,33 @@
 import json
+import requests
+import feedparser
 
 
 def handler(event, context):
+    RSS_URL = "https://investinglive.com/rss/"
     print(f"Event: {json.dumps(event)}")
+    try:
+        response = requests.get(RSS_URL, timeout=10)
+        response.raise_for_status()
+
+        feed = feedparser.parse(response.text)
+
+        for index, entry in feed.entries:
+            article = {
+                "title": entry.get("title"),
+                "link": entry.get("link"),
+                "published": entry.get("published"),
+                "summary": entry.get("summary"),
+                "id": entry.get("id"),
+            }
+            print(f"Article {index}: {json.dumps(article)}")
+    except requests.RequestException as e:
+        print(f"Error fetching RSS feed: {e}")
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"message": "Error fetching RSS feed"}),
+        }
+    
     # polling lambda
     return {
         "statusCode": 200,
