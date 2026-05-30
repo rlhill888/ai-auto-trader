eee2719268e85e471c25d87bcf3125ab158b651a
rlhill888 <rlhill888@gmail.com>
2026-05-29 22:17:01 -0400

    updating lambda to save articles to db

--- polling-article-lambda/lambda_function.py
+++ polling-article-lambda/lambda_function.py
@@ -1,6 +1,14 @@
 import json
+import os
+import boto3
 import requests
 import feedparser
+from botocore.exceptions import ClientError
+
+
+ARTICLE_TABLE_NAME = os.environ["TABLE_NAME"]
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(ARTICLE_TABLE_NAME)
 
 
 def lambda_handler(event, context):
@@ -23,7 +31,23 @@ def lambda_handler(event, context):
                 "summary": entry.get("summary"),
                 "id": entry.get("id"),
             }
-            print(f"Article {index}: {json.dumps(article)}")
+
+            try:
+                table.put_item(
+                    Item={
+                        "article-id": article["id"],
+                    },
+                    ConditionExpression="attribute_not_exists(#id)",
+                    ExpressionAttributeNames={"#id": "article-id"},
+                )
+                print(f"Article {index}: {json.dumps(article)}")
+                print(f"Saved article {index} to DynamoDB")
+            except ClientError as e:
+                if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
+                    print(f"Article {index} already exists, skipping")
+                else:
+                    raise
+
     except requests.RequestException as e:
         print(f"Error fetching RSS feed: {e}")
         return {
